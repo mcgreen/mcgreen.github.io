be9e25c990f1ecf890230281e42749c859f11ad1
Mike Green <mcgreen412@gmail.com>
2022-04-12 19:43:06 -0400

    config related

--- src/app/core/auth/auth.service.ts
+++ src/app/core/auth/auth.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 
 const CLIENT_ID = '9ce5e2c2c5d24431926e0519083ea029';
-const REDIRECT_URI = 'http://localhost:4200/callback';
+const REDIRECT_URI = 'http://localhost:4200/authorize';
 const SCOPE = 'user-read-private user-read-email';
 
 @Injectable({

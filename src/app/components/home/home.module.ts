df34d1ddfd162c23b5e890f70310a74ee1e32590
Mike Green <mcgreen412@gmail.com>
2022-04-18 22:28:10 -0400

    docs

--- src/app/components/home/home.module.ts
+++ src/app/components/home/home.module.ts
@@ -2,7 +2,6 @@ import {NgModule} from '@angular/core';
 
 import {HomeRoutingModule} from './home-routing.module';
 import {HomeComponent} from './home.component';
-import {NavModule} from "@components/nav/nav.module";
 import {NewReleasesModule} from "@components/new-releases/new-releases.module";
 import {SharedModule} from "@shared/shared.module";
 
@@ -14,7 +13,6 @@ import {SharedModule} from "@shared/shared.module";
   imports: [
     SharedModule,
     HomeRoutingModule,
-    NavModule,
     NewReleasesModule,
   ]
 })

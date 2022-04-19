0afa4e6879303473377aec230851d3229abac147
Mike Green <mcgreen412@gmail.com>
2022-04-18 22:53:51 -0400

    docs

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -25,7 +25,7 @@ const routes: Routes = [
   imports: [
     RouterModule.forRoot(routes),
   ],
-  exports: [RouterModule]
+  exports: []
 })
 export class AppRoutingModule {
 }
--- src/app/components/insights/insights-routing.module.ts
+++ src/app/components/insights/insights-routing.module.ts
@@ -11,7 +11,7 @@ const routes: Routes = [
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: []
 })
 export class InsightsRoutingModule {
 }
